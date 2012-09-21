4879e77b5203c14c3660c1653dc07bbd404bf597
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2012-09-21 20:52:29 +0200

    language patterns as hashes instead of slices

--- textcat.go
+++ textcat.go
@@ -3,16 +3,3 @@ A Go package for n-gram based text categorization, with support for utf-8 and ra
 */
 package textcat
 
-var (
-	LanguagesUtf8 = make([]string, 0)
-	LanguagesRaw  = make([]string, 0)
-)
-
-func init() {
-	for _, lang := range data {
-		LanguagesRaw = append(LanguagesRaw, lang.lang)
-		if len(lang.patUtf8) > 0 {
-			LanguagesUtf8 = append(LanguagesUtf8, lang.lang)
-		}
-	}
-}
