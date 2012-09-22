60e48c309b85c0da2681964e3409a542e670b779
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2012-09-22 15:46:23 +0200

    Added languages: af, fy

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+A Go package for n-gram based text categorization, with support for utf-8 and raw text.
+
+
+*/
+package textcat
--- textcat.go
+++ textcat.go
@@ -1,6 +1,3 @@
-/*
-A Go package for n-gram based text categorization, with support for utf-8 and raw text.
-*/
 package textcat
 
 import (
