05436e203b0fa1d7f2e6ef022e3be31c958a87b2
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2012-12-12 17:22:43 +0100

    doc.go: description of sources for language models

--- doc.go
+++ doc.go
@@ -1,6 +1,22 @@
 /*
 A Go package for n-gram based text categorization, with support for utf-8 and raw text.
 
+Many built-in language models are based on frequency lists, see:
+
+    http://en.wiktionary.org/wiki/Wiktionary:Frequency_lists
+
+Most of these lists are based on www.opensubtitles.org, see:
+
+    http://invokeit.wordpress.com/frequency-word-lists/
+    https://skydrive.live.com/?cid=3732e80b128d016f&id=3732E80B128D016F%213584
+
+Languages: af ar bg ca cs cy da de el en es et eu fa fi fr he hr hu id
+is it lt lv mk ms nl no pl pt ro ru sk sl sq sr sr-latin sv tr uk ur 
+
+
+Other language models are based on texts collected from Wikipedia.
+
+Languages: eo fy ga gd hi ia ja kk ko la lb sw ta tl vi vo yi yo zh
 
 */
 package textcat
